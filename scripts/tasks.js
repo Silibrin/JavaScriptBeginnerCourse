a357e227b25eb6af00b2cd531b5dcd32e03fee5b
Silibrin <kirilnikitin@gmail.com>
2016-12-09 11:17:25 +0300

    Only 1,2 and 3, don't hurt me >_<

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -5,12 +5,35 @@ var usersData = [
 	{ 'user' : 'Alex', 'password' : 'MyNameIsAlex' },
 	{ 'user' : 'Bob', 'password' : 'MyNAmeIsBob' }
 ];
+function getFieldValues(uData, fName){
+	var arr = [];
+	for(var i = 0; i < uData.length; i++){
+		arr[i] = uData[i][fName];
+	}
+	arr.sort();
+	return arr;
+}
 console.log(getFieldValues(usersData, 'user')); // --> ['Alex', 'Bob']
 
 // 2) Написать функцию, фильтрующую массив с использованием предиката:
 
 var numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
-function isEven(x) {/* Проверка на чётность */}
+function isEven(x) {
+	if (x % 2 == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+function filter(numb, pred){
+	var arr = [];
+	for(var i = 0; i < numb.length; i++){
+		if(pred(numb[i]) == true){
+			arr.push(numb[i]);
+		}
+	}
+	return arr;
+}
 console.log(filter(numbers, isEven)); // --> [2, 8, 34]
 
 // 3) Даны 2 строки со словами (без знаков препинания), 
@@ -18,6 +41,23 @@ console.log(filter(numbers, isEven)); // --> [2, 8, 34]
 
 var firstLongString = 'Load up on guns and bring your friends it\'s fun to lose and to pretend';
 var secondLongString = 'She\'s over bored and self assured oh no I know a dirty word';
+function findSimilarWords(firstString, secondString){
+	//создание массивов из строк
+	var firstArr = firstString.split(' ');
+	var secondArr = secondString.split(' ');
+	var outputArr = [];
+	for(var i = 0; i < firstArr.length; i++){
+		for(var j = 0; j < secondArr.length; j++){
+			if(firstArr[i] == secondArr[j]){
+				if(outputArr.indexOf(secondArr[i]) == -1){ //проверка на то, встречалось ли это слово ранее
+					outputArr.push(firstArr[i]);
+				}
+			}
+		}
+	}
+	return outputArr;
+}
+
 console.log(findSimilarWords(firstLongString, secondLongString)); // --> ['and'];
 
 // 4) Дан IP-адрес (строка) и маска подсети (десятичное число). Написать функцию, которая будет валидировать
